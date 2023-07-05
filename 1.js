6443544e5b7848c9b1e206cae0e10edd0dcca12b
faroksalman <136682886+faroksalman@users.noreply.github.com>
2023-07-05 18:57:38 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,18 @@
+let message = "You enter a dimly lit room.";
+let treasureCount = 0;
+let key = {
+  material: "gold",
+  carvings: "intricate",
+};
+let isTrapActivated = false;
+let secretPassage = null;
+let mysteriousArtifact = undefined;
+let ancientSymbol = Symbol("Ancient Symbol");
+
+console.log("ty of message : " + typeof message);
+console.log("ty of treasureCount : " + typeof treasureCount);
+console.log("ty of key : " + typeof key);
+console.log("ty of isTrapActivated : " + typeof isTrapActivated);
+console.log("ty of secretPassage : " + typeof secretPassage);
+console.log("ty of mysteriousArtifact : " + typeof mysteriousArtifact);
+console.log("ty of ancientSymbol : " + typeof ancientSymbol);
